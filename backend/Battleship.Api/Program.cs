887addedf0a003184509ac16461a6c611d351959
simone rotondi <rotondi.simone@gmail.com>
2026-06-10 22:39:55 -0400

    Add CORS

--- backend/Battleship.Api/Program.cs
+++ backend/Battleship.Api/Program.cs
@@ -6,6 +6,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173", "https://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,6 +28,8 @@ builder.Services.AddSingleton<GameFactory>();
 builder.Services.AddDbContext<BattleshipDbContext>(options => options.UseSqlite("Data Source=battleship.db"));
 
 var app = builder.Build();
+
+app.UseCors("Frontend");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
